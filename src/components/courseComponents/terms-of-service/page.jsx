0b3985ab3ad13ea84737b4ce422cc6ed8370ec3e
Divyansh Chauhan <divyansh20060@gmail.com>
2024-07-16 21:27:05 +0530

    Now payment are done and save in backend

--- src/components/courseComponents/terms-of-service/page.jsx
+++ src/components/courseComponents/terms-of-service/page.jsx
@@ -1,45 +1,25 @@
 import React from 'react';
 
-export default function TermsOfService() {
+const PdfViewer = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
       <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-8 p-8">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Terms of Service</h1>
         <div className="text-lg text-gray-700">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tortor ac odio
-            ultricies ullamcorper. Nulla facilisi. Vivamus et libero justo. Duis vitae nunc
-            scelerisque, tincidunt lorem nec, ultricies elit.
-          </p>
-          <p className="mt-4">
-            Vestibulum nec varius ex. Curabitur eu lacus consequat, efficitur neque at, eleifend
-            quam. Nunc euismod mauris eu dolor lacinia, vitae congue arcu placerat. Sed ac turpis
-            sed justo ullamcorper molestie non vitae tortor.
-          </p>
-          <ul className="list-disc mt-4">
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </li>
-            <li>
-              Nulla facilisi. Proin a diam id metus pulvinar efficitur.
-            </li>
-            <li>
-              Vivamus et libero justo. Phasellus feugiat urna vitae neque consequat.
-            </li>
-            <li>
-              Duis vitae nunc scelerisque, tincidunt lorem nec, ultricies elit.
-            </li>
-          </ul>
-          <p className="mt-4">
-            Phasellus semper ex quis libero efficitur ultrices. Etiam auctor purus eget mi dapibus,
-            vel dictum quam sollicitudin. Integer sit amet nisi tortor. Vestibulum tincidunt erat
-            sit amet posuere pharetra.
-          </p>
+          {/* Placeholder for other content */}
         </div>
         <div className="mt-6">
-        
+          <iframe
+            src={"https://firebasestorage.googleapis.com/v0/b/pulsezest.appspot.com/o/PulseZest-Course-Agreement%2FPulsezest%20Agreement%20(1).pdf?alt=media&token=06caddff-f0d2-46f2-b1f7-b45985215ab4"}
+            width="100%"
+            height="500px"
+            style={{ border: 'none' }}
+            allowFullScreen
+          />
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default PdfViewer;
